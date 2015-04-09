6ac0eba8d9df8bfb5c391e8b3c7c2f912abfc883
Srinivas <sairao1996@gmail.com>
2015-04-10 04:40:32 +0530

    Added comments

--- AndroidCameraViewer/src/com/example/androidcameraviewer/MainActivity.java
+++ AndroidCameraViewer/src/com/example/androidcameraviewer/MainActivity.java
@@ -31,21 +31,33 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-	private ImageView imag;
-	private Button btn;
+	/*Image View declared*/
+	private ImageView imag;		
+	/*Button declared*/
+	private Button btn;	
+	
 	private CameraPrx cprx;
+	/*String for port*/
 	private String port = "9999";
+	/*String for ip address*/
 	private String ipaddress = "";
+	/*String for protocol*/
 	private String protocol = "tcp";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        /*Set the imageview to imag*/
         imag = (ImageView) findViewById(R.id.imageView1);
+        
+        /*Set button
         btn = (Button)findViewById(R.id.Button); 
+        
         btn.setOnClickListener(this);
         
+        /*Call the preferences and set them to the strings*/
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         port = prefs.getString("Port Number", "9999");
         protocol = prefs.getString("protocol", "tcp");
@@ -64,6 +76,7 @@ public class MainActivity extends Activity implements OnClickListener {
 //    	        	 
 //    	        } 
 //    	    }, 0, 1000); 
+        //Initialize ICE Communicator
         try {
   			initializeCommunicator();
   			//Toast.makeText(getApplicationContext(), "Communicator initialized", Toast.LENGTH_LONG).show();
@@ -77,15 +90,25 @@ public class MainActivity extends Activity implements OnClickListener {
         
     }
 
-    public void onClick(View v) { // Parameter v stands for the view that was clicked.  
+    public void onClick(View v) {  
     	try {
+    		
   			jderobot.ImageData realdata;
+  			
+  			/*Get the image data*/
   	  		realdata = cprx.getImageData();
+  	  		
+  	  		/*Present image format is NV21 and it gives 240 x 160 size images*/
   	  		YuvImage img = new YuvImage(realdata.pixelData, ImageFormat.NV21, 240, 160, null);
+  	  		
+  	  		/*Convert NV21 image to Jpeg*/
   	  	  	ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
   	  	  	img.compressToJpeg(new Rect(0, 0, 240, 160), 50, baoStream);
+  	  	  	
+  	  	  	/*Convert image to Bitmap*/
   	  	  	Bitmap mBitmap = BitmapFactory.decodeByteArray(baoStream.toByteArray(),0,baoStream.size());
   	  	  	
+  	  	  	/*Set the image to ImageView*/
   	  	  	imag.setImageBitmap(mBitmap);
   	  	} catch (DataNotExistException e) {
   	  		// TODO Auto-generated catch block
@@ -100,32 +123,32 @@ public class MainActivity extends Activity implements OnClickListener {
   	
   	}
     
-    private Runnable mMyRunnable = new Runnable()
-    {
-        @Override
-        public void run()
-        {
-        	try {
-      			jderobot.ImageData realdata;
-      	  		realdata = cprx.getImageData();
-      	  		YuvImage img = new YuvImage(realdata.pixelData, ImageFormat.NV21, 240, 160, null);
-      	  	  	ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
-      	  	  	img.compressToJpeg(new Rect(0, 0, 240, 160), 50, baoStream);
-      	  	  	Bitmap mBitmap = BitmapFactory.decodeByteArray(baoStream.toByteArray(),0,baoStream.size());
-      	  	  	
-      	  	  	imag.setImageBitmap(mBitmap);
-      	  	} catch (DataNotExistException e) {
-      	  		// TODO Auto-generated catch block
-      	  		e.printStackTrace();
-      	  	} catch (HardwareFailedException e) {
-      	  		// TODO Auto-generated catch block
-      	  		e.printStackTrace();
-      	  		} catch (Exception e){
-      	  			e.printStackTrace();
-      	  		}
-        }
-        
-     };
+//    private Runnable mMyRunnable = new Runnable()
+//    {
+//        @Override
+//        public void run()
+//        {
+//        	try {
+//      			jderobot.ImageData realdata;
+//      	  		realdata = cprx.getImageData();
+//      	  		YuvImage img = new YuvImage(realdata.pixelData, ImageFormat.NV21, 240, 160, null);
+//      	  	  	ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
+//      	  	  	img.compressToJpeg(new Rect(0, 0, 240, 160), 50, baoStream);
+//      	  	  	Bitmap mBitmap = BitmapFactory.decodeByteArray(baoStream.toByteArray(),0,baoStream.size());
+//      	  	  	
+//      	  	  	imag.setImageBitmap(mBitmap);
+//      	  	} catch (DataNotExistException e) {
+//      	  		// TODO Auto-generated catch block
+//      	  		e.printStackTrace();
+//      	  	} catch (HardwareFailedException e) {
+//      	  		// TODO Auto-generated catch block
+//      	  		e.printStackTrace();
+//      	  		} catch (Exception e){
+//      	  			e.printStackTrace();
+//      	  		}
+//        }
+//        
+//     };
   	
     /* Implementation of ICE */
     interface CommunicatorCallback {
@@ -141,21 +164,29 @@ public class MainActivity extends Activity implements OnClickListener {
 
     private void initializeCommunicator() throws DataNotExistException, HardwareFailedException {
       try {
+    	/*Initialize Ice communicator*/
         Ice.Communicator communicator;
         communicator = Ice.Util.initialize();
+        
+        /*Get the object proxy*/
       	Ice.ObjectPrx base = communicator.stringToProxy("cameraA:"+protocol+ " -h "+ipaddress+" -p " + port);
       	
       	cprx = jderobot.CameraPrxHelper.checkedCast(base);
       	
+      	/*Get image data*/
       	jderobot.ImageData realdata = cprx.getImageData();
       	//Toast.makeText(getApplicationContext(), realdata.toString(), Toast.LENGTH_LONG).show();
       	//jderobot.ImageData realdata = cprx.begin_getImageData();
       	
+      	/*Convert NV21 image data to Jpeg*/
       	YuvImage img = new YuvImage(realdata.pixelData, ImageFormat.NV21, 240, 160, null);
       	ByteArrayOutputStream baoStream = new ByteArrayOutputStream();
       	img.compressToJpeg(new Rect(0, 0, 240, 160), 50, baoStream);
+      	
+      	/*Convert Jpeg to Bitmap*/
       	Bitmap mBitmap = BitmapFactory.decodeByteArray(baoStream.toByteArray(),0,baoStream.size());
       	
+      	/*Set Bitmap image*/
       	imag.setImageBitmap(mBitmap);
       	//Toast.makeText(getApplicationContext(), mBitmap.toString(), Toast.LENGTH_LONG).show();
         
@@ -194,7 +225,7 @@ public class MainActivity extends Activity implements OnClickListener {
       /* We call the Preferences and get the selected values */
       SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-      // Get the port and protocol
+      /* Get the port, protocol and ip address */
       port = prefs.getString("Port Number", "9999");
       protocol = prefs.getString("protocol", "default");
       ipaddress = prefs.getString("ipkey", "172.10.2.102");
